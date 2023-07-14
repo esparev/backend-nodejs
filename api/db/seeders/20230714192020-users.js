436787d46bfbb5177b75253d47cf725e2f59c35d
JoseMa <esparev@hotmail.com>
2023-07-14 16:04:59 -0600

    refactor: ♻️ modificar campos de las semillas

--- api/db/seeders/20230714192020-users.js
+++ api/db/seeders/20230714192020-users.js
@@ -6,7 +6,6 @@ module.exports = {
 	async up(queryInterface, Sequelize) {
 		await queryInterface.bulkInsert(USER_TABLE, [
 			{
-				id: 1,
 				name: 'Jose Maria',
 				email: 'esparev@hotmail.com',
 				password:
