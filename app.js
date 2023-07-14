9d83ba8c2a6723a5a01b210cb3a0480d21e61cd1
JoseMa <esparev@hotmail.com>
2023-07-14 14:33:37 -0600

    feat: ✨ implementar las diferentes capas del modelo de negocio

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const passport = require('passport');
+const routerApi = require('./api/routes');
+
+const {
+	errorHandler,
+	ormErrorHandler,
+	boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const createApp = () => {
 	const app = express();
 
+	const limiter = rateLimit({
+		windowMs: 15 * 1000, // 15 seconds
+		max: 100, // limit each IP to 100 requests per windowMs
+	});
+
+	require('./utils/auth');
+
+	app.use(limiter);
+	app.use(passport.initialize());
 	app.use(express.json());
-	app.use(bodyParser.json());
+	app.use(bodyParser.json({ limit: '5mb' }));
+
+	routerApi(app);
+
+	app.use(boomErrorHandler);
+	app.use(ormErrorHandler);
+	app.use(errorHandler);
 
 	return app;
 };
