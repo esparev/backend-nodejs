f3889818090692bb749e4488a71c2a0b98c77f27
JoseMa <esparev@hotmail.com>
2023-07-14 14:28:31 -0600

    auth: 🛂 crear estrategias de autenticacion con passport

---
+++ utils/auth/index.js
@@ -0,0 +1,7 @@
+// @ts-check
+const passport = require('passport');
+const LocalStrategy = require('./strategies/local.strategy');
+const JwtStrategy = require('./strategies/jwt.strategy');
+// Passport middlewares to use the existing strategies
+passport.use(LocalStrategy);
+passport.use(JwtStrategy);
